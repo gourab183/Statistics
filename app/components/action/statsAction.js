6eaa7dbef0e225db49e2bd3fef8e27bdaa4d6f13
gourab183 <gourab092@gmail.com>
2020-06-28 20:48:10 +0530

    change dependency

--- app/components/action/statsAction.js
+++ app/components/action/statsAction.js
@@ -5,7 +5,7 @@ import queryString from 'querystring';
 function fetchStats(pageNo) {
     return async function (dispatch) {
         try {
-            const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=foo&tags=story&page=${pageNo}`);
+            const response = await axios.get(`https://cors-anywhere.herokuapp.com/http://hn.algolia.com/api/v1/search?query=foo&tags=story&page=${pageNo}`);
             dispatch({
                 type: STATS_TYPE,
                 data: response.data.hits
