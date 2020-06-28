ba06bca42ce4f384edd9ed4058330a78d21d7eba
gourab183 <gourab092@gmail.com>
2020-06-28 19:05:20 +0530

    ready for heroku chanr port

--- server/index.js
+++ server/index.js
@@ -13,6 +13,6 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', render.default);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Listening on port ${port}`);
